89b1676af51bdc6c4d97b0660cedb0d484991688
ThunderFound <90287659+ThunderFound@users.noreply.github.com>
2024-04-08 16:18:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Находим кнопку по id и добавляем обработчик события при клике
+document.getElementById('generate').addEventListener('click', function() {
+    // Находим input и textarea по id
+    const textLengthInput = document.getElementById('text_length');
+    const textarea = document.getElementById('textarea');
+    
+    // Получаем значение длины текста из input и преобразуем его в число
+    const textLength = parseInt(textLengthInput.value);
+
+    // Строка символов, из которых будем генерировать текст
+    const characters = 'йцукенгшщзхъфывапролджэячсмитьбю    ,..,.,.()1234567890😭😰🥵🤪😱🤑💀';
+
+    // Генерируем случайный текст заданной длины
+    let generatedText = '';
+    for (let i = 0; i < textLength; i++) {
+        generatedText += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    // Устанавливаем сгенерированный текст в textarea
+    textarea.value = generatedText;
+});
